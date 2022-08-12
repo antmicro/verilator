c3700e40d1f1aed66edb8a5afd653dc95dbea5bf
Ryszard Rozak <rrozak@antmicro.com>
2022-08-12 15:59:57 +0200

    Add Antmicro 2022 to descriptions of tests
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_tri_cast_select_eqcase.v
+++ test_regress/t/t_tri_cast_select_eqcase.v
@@ -1,7 +1,7 @@
 // DESCRIPTION: Verilator: Verilog Test module
 //
 // This file ONLY is placed under the Creative Commons Public Domain, for
-// any use, without warranty, 2013 by Wilson Snyder.
+// any use, without warranty, 2022 by Antmicro Ltd.
 // SPDX-License-Identifier: CC0-1.0
 
 module t (/*AUTOARG*/);

KingkingLiu/verilator-1

Mark several functions as VL_MT_SAFE
Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

src/V3Error.h
@@ -432,7 +432,7 @@ inline void v3errorEndFatal(std::ostringstream& sstr) {
 
 // Helper macros for VL_DEFINE_DEBUG_FUNCTIONS
 #define VL_DEFINE_DEBUG(name) \
-    VL_ATTR_UNUSED static int debug##name() { \
+    VL_ATTR_UNUSED static int debug##name() VL_MT_SAFE { \
         static int level = -1; \
         if (VL_UNLIKELY(level < 0)) { \
             std::string tag{VL_STRINGIFY(name)}; \

antmicro/verilator

src/V3Error.h
@@ -642,7 +642,7 @@ inline void v3errorEndFatal(std::ostringstream& sstr)
 // Helper macros for VL_DEFINE_DEBUG_FUNCTIONS
 // Takes an optional "name" (as __VA_ARGS__)
 #define VL_DEFINE_DEBUG(...) \
-    VL_ATTR_UNUSED static int debug##__VA_ARGS__() { \
+    VL_ATTR_UNUSED static int debug##__VA_ARGS__() VL_MT_SAFE { \
         static int level = -1; \
         if (VL_UNLIKELY(level < 0)) { \
             std::string tag{VL_STRINGIFY(__VA_ARGS__)}; \
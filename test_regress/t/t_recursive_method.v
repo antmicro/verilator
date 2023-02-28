b817df93461427ae1448a1cbeac70e41e95b9563
Ryszard Rozak <rrozak@antmicro.com>
2023-02-28 09:39:35 +0100

    Add test
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_recursive_method.v
+++ test_regress/t/t_recursive_method.v
@@ -22,10 +22,33 @@ class FibStatic;
    endfunction
 endclass
 
+class Factorial;
+   static function int factorial(int n);
+      return fact(n, 1);
+   endfunction
+   static function int fact(int n, int acc);
+      if (n < 2)
+         fact = acc;
+      else
+         fact = fact(n - 1, acc * n);
+   endfunction
+endclass
+
+class Getter3 #(int T=5);
+   static function int get_3();
+      if (T == 3)
+        return 3;
+      else
+        return Getter3#(3)::get_3();
+   endfunction
+endclass
+
 module t (/*AUTOARG*/);
 
    initial begin
       Fib fib = new;
+      Getter3 getter3 = new;
+
       if (fib.get_fib(0) != 0) $stop;
       if (fib.get_fib(1) != 1) $stop;
       if (fib.get_fib(8) != 21) $stop;
@@ -33,6 +56,13 @@ module t (/*AUTOARG*/);
       if (FibStatic::get_fib(0) != 0) $stop;
       if (FibStatic::get_fib(1) != 1) $stop;
       if (FibStatic::get_fib(8) != 21) $stop;
+
+      if (Factorial::factorial(0) != 1) $stop;
+      if (Factorial::factorial(1) != 1) $stop;
+      if (Factorial::factorial(6) != 720) $stop;
+
+      if (getter3.get_3() != 3) $stop;
+      if (Getter3#(3)::get_3() != 3) $stop;
 
       $write("*-* All Finished *-*\n");
       $finish;

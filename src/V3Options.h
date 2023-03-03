KingkingLiu/verilator-1

Mark several functions as VL_MT_SAFE
Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

src/V3Options.h
@@ -644,7 +644,7 @@ public:
     }
 
     // METHODS (from main)
-    static string version();
+    static string version() VL_MT_SAFE;
     static string argString(int argc, char** argv);  ///< Return list of arguments as simple string
     string allArgsString() const VL_MT_SAFE;  ///< Return all passed arguments as simple string
     // Return options for child hierarchical blocks when forTop==false, otherwise returns args for

antmicro/verilator

src/V3Options.h
@@ -646,7 +646,7 @@ public:
     }
 
     // METHODS (from main)
-    static string version();
+    static string version() VL_PURE;
     static string argString(int argc, char** argv);  ///< Return list of arguments as simple string
     string allArgsString() const VL_MT_SAFE;  ///< Return all passed arguments as simple string
     // Return options for child hierarchical blocks when forTop==false, otherwise returns args for
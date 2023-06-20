0381c5423d92c3ca740ef7ff91ee72e06dd51338
Ryszard Rozak <rrozak@antmicro.com>
2023-06-20 20:10:07 +0200

    Fix handling of ref types in initial values of type parameters (#4304)

--- src/V3AstNodeDType.h
+++ src/V3AstNodeDType.h
@@ -1076,6 +1076,7 @@ public:
     AstRefDType(FileLine* fl, FlagTypeOfExpr, AstNode* typeofp)
         : ASTGEN_SUPER_RefDType(fl) {
         this->typeofp(typeofp);
+        if (AstNodeDType* const dtp = VN_CAST(typeofp, NodeDType)) refDTypep(dtp);
     }
     ASTGEN_MEMBERS_AstRefDType;
     // METHODS

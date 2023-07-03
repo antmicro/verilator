caf7d7251bf62fdad78cae70c31843f09e0eef6d
Ryszard Rozak <rrozak@antmicro.com>
2023-07-03 15:43:12 +0200

    Remove unused variable, fix formatting
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3EmitCFunc.h
+++ src/V3EmitCFunc.h
@@ -273,11 +273,11 @@ public:
             const AstClass* const classp = VN_CAST(nodep->scopep()->modp(), Class);
             bool baseCtorCall = false;
             // Find call to super.new to get the arguments
-                    if (classp && constructorNeedsProcess(classp)) {
-                        puts("(vlProcess, vlSymsp");
-                    } else {
-                        puts("(vlSymsp");
-                    }
+            if (classp && constructorNeedsProcess(classp)) {
+                puts("(vlProcess, vlSymsp");
+            } else {
+                puts("(vlSymsp");
+            }
             const AstCNew* const superNewCallp = getSuperNewCallRecursep(nodep->stmtsp());
             UASSERT_OBJ(superNewCallp, nodep, "super.new call not found");
             putCommaIterateNext(superNewCallp->argsp(), true);

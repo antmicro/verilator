8a225b398bdf16dc55f49663e15a3c049671f008
Ryszard Rozak <rrozak@antmicro.com>
2023-07-13 11:30:37 +0200

    Change error message
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3Task.cpp
+++ src/V3Task.cpp
@@ -1346,7 +1346,7 @@ private:
     AstNode* insertBeforeStmt(AstNode* nodep, AstNode* newp) {
         // Return node that must be visited, if any
         if (debug() >= 9) nodep->dumpTree("-  newstmt: ");
-        UASSERT_OBJ(m_insStmtp, nodep, "Function not underneath a statement");
+        UASSERT_OBJ(m_insStmtp, nodep, "Function call not underneath a statement");
         AstNode* visitp = nullptr;
         if (m_insMode == IM_BEFORE) {
             // Add the whole thing before insertAt

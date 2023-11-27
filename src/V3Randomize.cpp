37cfb94fcd25405e3bd6de379c9fa174de4d32b6
Aleksander Kiryk <akiryk@antmicro.com>
2023-11-27 14:58:53 +0100

    Remove unused var declaration

--- src/V3Randomize.cpp
+++ src/V3Randomize.cpp
@@ -351,7 +351,6 @@ private:
                                 assignp};
                 funcp->addStmtsp(assignIfNotNullp);
             } else if (const auto* const dynp = VN_CAST(dtypep, DynArrayDType)) {
-                AstVarRef* const refp = new AstVarRef{fl, memberVarp, VAccess::WRITE};
                 AstNodeExpr* callp = newRandValue(fl, memberVarp, memberVarp->findBasicDType(VBasicDTypeKwd::UINT32));
                 AstAssign* const assignp = new AstAssign{
                     fl, new AstVarRef{fl, fvarp, VAccess::WRITE},

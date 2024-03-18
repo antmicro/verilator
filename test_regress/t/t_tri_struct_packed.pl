ac146549f93df9e12276e419cc3baaa9b0f105af
Ryszard Rozak <rrozak@antmicro.com>
2024-03-18 17:47:37 +0100

    Update a test
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_tri_struct_packed.pl
+++ test_regress/t/t_tri_struct_packed.pl
@@ -2,21 +2,20 @@
 if (!$::Driver) { use FindBin; exec("$FindBin::Bin/bootstrap.pl", @ARGV, $0); die; }
 # DESCRIPTION: Verilator: Verilog Test driver/expect definition
 #
-# Copyright 2021 by Wilson Snyder. This program is free software; you
+# Copyright 2024 by Wilson Snyder. This program is free software; you
 # can redistribute it and/or modify it under the terms of either the GNU
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
-scenarios(simulator => 1);
+scenarios(simulator_st => 1);
 
 compile(
     verilator_flags2 => ['--exe --main --timing'],
     );
 
 execute(
-    fails => $Self->{vlt_all},
-    expect_filename => $Self->{golden_filename},
+    check_finished => 1,
     );
 
 ok(1);

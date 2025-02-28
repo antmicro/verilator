2f2a2aa826fc25e6a760867998ab406b08f91603
Bartłomiej Chmiel <bchmiel@antmicro.com>
2025-02-28 16:34:48 +0100

    Support multiple setuphold statements in one block
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- test_regress/t/t_setuphold.v
+++ test_regress/t/t_setuphold.v
@@ -1,3 +1,9 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Antmicro.
+// SPDX-License-Identifier: CC0-1.0
+
 module t (/*AUTOARG*/
    // Inputs
    clk,
@@ -10,11 +16,24 @@ module t (/*AUTOARG*/
    wire delayed_D;
    reg notifier;
 
-   specify
-      $setuphold (posedge clk, negedge d, 0, 0, notifier,,, delayed_CLK, delayed_D);
-   endspecify
+   logic[3:0] sh1 = 1;
+   logic[3:0] sh2 = 2;
+   logic[3:0] sh3 = 3;
+   logic[3:0] sh4 = 4;
 
    int cyc = 0;
+
+   specify
+      $setuphold (posedge clk, negedge d, 0, 0, notifier,,, delayed_CLK, delayed_D);
+      $setuphold (posedge sh1, negedge sh3, 0, 0, notifier,,, sh2, sh4);
+      $setuphold (posedge clk, negedge d, 0, 0);
+   endspecify
+
+   initial begin
+      if (sh1 != sh2 || sh3 != sh4) begin
+         $stop;
+      end
+   end
 
    always @(posedge clk) begin
       cyc <= cyc + 1;

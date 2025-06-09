e0277170a316e7d8760b026cb4619305785a0036
Bartłomiej Chmiel <bchmiel@antmicro.com>
2025-06-09 16:03:48 +0200

    Test
    Signed-off-by: Bartłomiej Chmiel <bchmiel@antmicro.com>

--- test_regress/t/t_udp_sequential.v
+++ test_regress/t/t_udp_sequential.v
@@ -4,6 +4,9 @@
 // without warranty, 2015 by Mike Thyer.
 // SPDX-License-Identifier: CC0-1.0
 
+`define stop $stop
+`define checkh(gotv,expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%x exp='h%x\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0)
+
 primitive d_edge_ff (q, clock, data);
  output q; reg q;
  input clock, data;
@@ -15,6 +18,7 @@ primitive d_edge_ff (q, clock, data);
      (10) 1 : ? : 1 ;
      R    0 : ? : 1 ;
      (0?) 1 : ? : 0 ;
+     //? (??) : ? : - ;
  endtable
 endprimitive
 
@@ -23,44 +27,47 @@ module t (/*AUTOARG*/
    clk
    );
    input clk;
+   //logic clk = 0; initial forever clk = #1 ~clk;
    reg d, q;
    d_edge_ff g (q, clk, d);
 
    int cycle=0;
    initial d = 0;
    always @(posedge clk or negedge clk) begin
+        $display("d=%d q=%d clk=%d cycle=%0d", d, q, clk, cycle);
      cycle <= cycle+1;
      if (cycle==0) begin
         d = 1;
+        d = 0;
      end
      else if (cycle==1) begin
-        d = 0;
-        if (q != 1) $stop;
+        `checkh(q, 1);
      end
      else if (cycle==2) begin
-        if (q != 1) $stop;
+        `checkh(q, 0);
      end
      else if (cycle==3) begin
-        if (q != 0) $stop;
+        `checkh(q, 1);
      end
      else if (cycle==4) begin
         d = 1;
-        if (q != 1) $stop;
+        `checkh(q, 0);
      end
      else if (cycle==5) begin
-        $display("d=%d clk=%d cycle=%0d", d, clk, cycle);
-        if (q != 1) $stop;
+        $display("d=%d q=%d clk=%d cycle=%0d", d, q, clk, cycle);
+        `checkh(q, 0);
      end
      else if (cycle==6) begin
-        if (q != 0) $stop;
+        `checkh(q, 1);
      end
      else if (cycle==7) begin
-        if (q != 1) $stop;
+        `checkh(q, 0);
      end
      else if (cycle >= 8) begin
-        if (q != 0) $stop;;
+        `checkh(q, 1);
         $write("*-* All Finished *-*\n");
         $finish;
      end
    end
 endmodule
+

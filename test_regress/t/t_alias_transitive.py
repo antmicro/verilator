eeb29c6db073a830e4bcb3c7266cf408740e6615
Ryszard Rozak <rrozak@antmicro.com>
2025-08-25 15:18:53 +0200

    Expect pass in t_alias_transitive
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- test_regress/t/t_alias_transitive.py
+++ test_regress/t/t_alias_transitive.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # DESCRIPTION: Verilator: Verilog Test driver/expect definition
 #
-# Copyright 2025 by Wilson Snyder. This program is free software; you can
-# redistribute it and/or modify it under the terms of either the GNU
+# Copyright 2024 by Wilson Snyder. This program is free software; you
+# can redistribute it and/or modify it under the terms of either the GNU
 # Lesser General Public License Version 3 or the Perl Artistic License
 # Version 2.0.
 # SPDX-License-Identifier: LGPL-3.0-only OR Artistic-2.0
 
 import vltest_bootstrap
 
-test.scenarios('vlt')
+test.scenarios('simulator')
 
-test.top_filename = "t/t_alias_transitive.v"
+test.compile()
 
-test.lint()
+test.execute()
 
 test.passes()

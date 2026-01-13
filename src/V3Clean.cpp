4b87d8540606ec362dba30f39b2663bd855671ad
Ryszard Rozak <rrozak@antmicro.com>
2026-01-13 12:32:36 +0100

    Don't change dtype of ConsPackUOrStruct
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3Clean.cpp
+++ src/V3Clean.cpp
@@ -82,6 +82,7 @@ class CleanVisitor final : public VNVisitor {
         if (!nodep->user2() && nodep->hasDType()) {
             if (VN_IS(nodep, Var)  //
                 || VN_IS(nodep, ConsPackMember)  //
+                || VN_IS(nodep, ConsPackUOrStruct)  //
                 || VN_IS(nodep, NodeDType)  // Don't want to change variable widths!
                 || VN_IS(nodep->dtypep()->skipRefp(), AssocArrayDType)  // Or arrays
                 || VN_IS(nodep->dtypep()->skipRefp(), WildcardArrayDType)

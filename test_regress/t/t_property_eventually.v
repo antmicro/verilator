ff89d75764463d9da3869956364adf12fd0af8d6
Ryszard Rozak <rrozak@antmicro.com>
2026-05-15 10:26:54 +0200

    Add test
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

---
+++ test_regress/t/t_property_eventually.v
@@ -0,0 +1,42 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2026 Antmicro
+// SPDX-License-Identifier: CC0-1.0
+
+`define stop $stop
+`define checkh(gotv,
+               expv) do if ((gotv) !== (expv)) begin $write("%%Error: %s:%0d:  got='h%p exp='h%p\n", `__FILE__,`__LINE__, (gotv), (expv)); `stop; end while(0)
+
+module t (  /*AUTOARG*/
+    // Inputs
+    clk
+);
+
+  input clk;
+
+  typedef struct {
+    int fails;
+    int passs;
+  } result_t;
+
+  result_t results[int];
+  result_t expected[int];
+
+  localparam MAX = 15;
+  integer cyc = 1;
+
+  assert property (@(posedge clk) eventually [1:2] 0)
+    results[1].passs++;
+  else results[1].fails++;
+
+  always @(edge clk) begin
+    ++cyc;
+    if (cyc == MAX) begin
+      expected[1] = '{5, 0};
+      `checkh(results, expected);
+      $write("*-* All Finished *-*\n");
+      $finish;
+    end
+  end
+endmodule

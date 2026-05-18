851d10f52f0896b125af893cce623384c2cd018e
Artur Bieniek <abieniek@antmicro.com>
2026-05-18 15:55:01 +0200

    Don't use arrays at all in t_property_until
    Signed-off-by: Artur Bieniek <abieniek@antmicro.com>

--- test_regress/t/t_property_until.v
+++ test_regress/t/t_property_until.v
@@ -20,84 +20,123 @@ module t (  /*AUTOARG*/
     int passs;
   } result_t;
 
-  // Must not be assoc-array to avoid races
-  result_t results[14];
-  result_t expected[14];
+  // TODO: make arrays mt-safe and use them instead of separate variables
+  result_t result1;
+  result_t result2;
+  result_t result3;
+  result_t result4;
+  result_t result5;
+  result_t result6;
+  result_t result7;
+  result_t result8;
+  result_t result9;
+  result_t result10;
+  result_t result11;
+  result_t result12;
+  result_t result13;
+
+  result_t expected1;
+  result_t expected2;
+  result_t expected3;
+  result_t expected4;
+  result_t expected5;
+  result_t expected6;
+  result_t expected7;
+  result_t expected8;
+  result_t expected9;
+  result_t expected10;
+  result_t expected11;
+  result_t expected12;
+  result_t expected13;
 
   localparam MAX = 15;
   integer cyc = 1;
 
   assert property (@(posedge clk) 0 until 1)
-    results[1].passs++;
-  else results[1].fails++;
+    result1.passs++;
+  else result1.fails++;
 
   assert property (@(posedge clk) 1 until 0)
-    results[2].passs++;
-  else results[2].fails++;
+    result2.passs++;
+  else result2.fails++;
 
   assert property (@(posedge clk) cyc < 5 until cyc >= 5)
-    results[3].passs++;
-  else results[3].fails++;
+    result3.passs++;
+  else result3.fails++;
 
   assert property (@(posedge clk) cyc % 3 == 0 until cyc % 5 == 0)
-    results[4].passs++;
-  else results[4].fails++;
+    result4.passs++;
+  else result4.fails++;
 
   assert property (@(posedge clk) cyc % 3 != 0 until_with cyc % 4 != 0)
-    results[5].passs++;
-  else results[5].fails++;
+    result5.passs++;
+  else result5.fails++;
 
   assert property (@(posedge clk) 0 s_until 1)
-    results[6].passs++;
-  else results[6].fails++;
+    result6.passs++;
+  else result6.fails++;
 
   assert property (@(posedge clk) cyc < 5 s_until cyc >= 5)
-    results[7].passs++;
-  else results[7].fails++;
+    result7.passs++;
+  else result7.fails++;
 
   assert property (@(posedge clk) cyc % 3 == 0 s_until cyc % 5 == 0)
-    results[8].passs++;
-  else results[8].fails++;
+    result8.passs++;
+  else result8.fails++;
 
   // Check that s_until accepts immediately when RHS is true, even if LHS is false.
   assert property (@(posedge clk) cyc % 2 == 0 s_until 1)
-    results[9].passs++;
-  else results[9].fails++;
+    result9.passs++;
+  else result9.fails++;
 
   // Check that s_until_with requires LHS when RHS is true on the same tick.
   assert property (@(posedge clk) 0 s_until_with 1)
-    results[10].passs++;
-  else results[10].fails++;
+    result10.passs++;
+  else result10.fails++;
 
   assert property (@(posedge clk) 1 s_until_with cyc >= 5)
-    results[11].passs++;
-  else results[11].fails++;
+    result11.passs++;
+  else result11.fails++;
 
   assert property (@(posedge clk) cyc <= 5 s_until_with cyc >= 5)
-    results[12].passs++;
-  else results[12].fails++;
+    result12.passs++;
+  else result12.fails++;
 
   assert property (@(posedge clk) cyc < 5 s_until_with cyc >= 5)
-    results[13].passs++;
-  else results[13].fails++;
+    result13.passs++;
+  else result13.fails++;
 
   always @(edge clk) begin
     ++cyc;
     if (cyc == MAX) begin
-      expected[1] = '{0, 7};
-      // expected[2] shouldn't be initialized
-      expected[3] = '{0, 7};
-      expected[4] = '{5, 2};
-      expected[5] = '{2, 5};
-      expected[6] = '{0, 7};
-      expected[7] = '{0, 7};
-      expected[8] = '{5, 2};
-      expected[9] = '{0, 7};
-      expected[10] = '{7, 0};
-      expected[11] = '{0, 7};
-      expected[12] = '{4, 3};
-      expected[13] = '{7, 0};
-      `checkh(results, expected);
+      expected1 = '{0, 7};
+      // expected2 shouldn't be initialized
+      expected3 = '{0, 7};
+      expected4 = '{5, 2};
+      expected5 = '{2, 5};
+      expected6 = '{0, 7};
+      expected7 = '{0, 7};
+      expected8 = '{5, 2};
+      expected9 = '{0, 7};
+      expected10 = '{7, 0};
+      expected11 = '{0, 7};
+      expected12 = '{4, 3};
+      expected13 = '{7, 0};
+
+      `checkh(result1, expected1);
+      `checkh(result2, expected2);
+      `checkh(result3, expected3);
+      `checkh(result4, expected4);
+      `checkh(result5, expected5);
+      `checkh(result6, expected6);
+      `checkh(result7, expected7);
+      `checkh(result8, expected8);
+      `checkh(result9, expected9);
+      `checkh(result10, expected10);
+      `checkh(result11, expected11);
+      `checkh(result12, expected12);
+      `checkh(result13, expected13);
+
       $write("*-* All Finished *-*\n");
       $finish;
     end

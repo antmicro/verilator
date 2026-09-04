588100ba63cc9903f2f7b04c1e7a8e4c3bfd1435
Ryszard Rozak <rrozak@antmicro.com>
2026-09-04 13:55:26 +0200

    Handle size vars
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/V3Randomize.cpp
+++ src/V3Randomize.cpp
@@ -4464,7 +4464,9 @@ class RandomizeVisitor final : public VNVisitor {
         if (derefFromp) {
             sizeNodeExprp = buildMemberSelExprp(derefFromp, sizeVarp);
         } else {
-            sizeNodeExprp = new AstVarRef{fl, sizeVarp, VAccess::READ};
+            AstVarRef* const sizeVarRefp = new AstVarRef{fl, sizeVarp, VAccess::READ};
+            sizeVarRefp->classOrPackagep(VN_AS(sizeVarp->user2p(), NodeModule));
+            sizeNodeExprp = sizeVarRefp;
         }
         sizeNodeExprp->user1(true);
         AstGteS* const sizeGtep = new AstGteS{fl, sizeNodeExprp, new AstConst{fl, 0}};
@@ -4578,33 +4580,31 @@ class RandomizeVisitor final : public VNVisitor {
             if (!ftaskp || !ftaskp->stmtsp()) return;
             for (AstNode* stmtp = ftaskp->stmtsp(); stmtp; stmtp = stmtp->nextp()) {
                 bool foundClearConstraints = false;
-                AstNode* clonedStmtp = nullptr;
                 stmtp->foreach([&](AstCMethodHard* methodp) {
-                    if (methodp->method() == VCMethod::RANDOMIZER_WRITE_VAR) {
-                        bool isStaticVar = false;
-                        methodp->pinsp()->foreach([&](AstNodeVarRef* refp) {
-                            if (refp->varp()->lifetime().isStatic()) isStaticVar = true;
-                        });
-                        if (isStaticVar) return;
-                        if (!clonedStmtp) clonedStmtp = stmtp->cloneTree(false);
-                        clonedStmtp->foreach([](AstCMethodHard* updateMethodp) {
-                            if (updateMethodp->method() != VCMethod::RANDOMIZER_WRITE_VAR) return;
-                            updateMethodp->method(VCMethod::RANDOMIZER_UPDATE_VAR);
-
-                            AstNodeExpr* const varp = updateMethodp->pinsp();
-                            AstNodeExpr* const widthp = VN_AS(varp->nextp(), NodeExpr);
-                            AstNodeExpr* const namep = VN_AS(widthp->nextp(), NodeExpr);
-                            varp->unlinkFrBack();
-                            namep->unlinkFrBack();
-                            updateMethodp->pinsp()->unlinkFrBackWithNext()->deleteTree();
-                            updateMethodp->addPinsp(varp);
-                            updateMethodp->addPinsp(namep);
-                        });
-                    } else if (methodp->method() == VCMethod::RANDOMIZER_CLEARCONSTRAINTS) {
+                    if (methodp->method() == VCMethod::RANDOMIZER_CLEARCONSTRAINTS) {
                         foundClearConstraints = true;
                     }
+                    if (methodp->method() != VCMethod::RANDOMIZER_WRITE_VAR) return;
+
+                    bool isStaticVar = false;
+                    methodp->pinsp()->foreach([&](AstNodeVarRef* refp) {
+                        if (refp->varp()->lifetime().isStatic()) isStaticVar = true;
+                    });
+                    if (isStaticVar) return;
+
+                    AstCMethodHard* const updateMethodp = methodp->cloneTree(false);
+                    updateMethodp->method(VCMethod::RANDOMIZER_UPDATE_VAR);
+
+                    AstNodeExpr* const varp = updateMethodp->pinsp();
+                    AstNodeExpr* const widthp = VN_AS(varp->nextp(), NodeExpr);
+                    AstNodeExpr* const namep = VN_AS(widthp->nextp(), NodeExpr);
+                    varp->unlinkFrBack();
+                    namep->unlinkFrBack();
+                    updateMethodp->pinsp()->unlinkFrBackWithNext()->deleteTree();
+                    updateMethodp->addPinsp(varp);
+                    updateMethodp->addPinsp(namep);
+                    updatep->addStmtsp(updateMethodp->makeStmt());
                 });
-                if (clonedStmtp) updatep->addStmtsp(clonedStmtp);
                 if (foundClearConstraints) break;
             }
         };
@@ -6375,9 +6375,10 @@ class RandomizeVisitor final : public VNVisitor {
                             = newResizeConstrainedArrayTask(classp, m_constraintp->name());
                         m_constraintp->user3p(resizerTaskp);
                     }
+                    AstVarRef* const sizeVarRefp = new AstVarRef{fl, sizeVarp, VAccess::READ};
+                    sizeVarRefp->classOrPackagep(VN_AS(sizeVarp->user2p(), NodeModule));
                     AstCMethodHard* const resizep = new AstCMethodHard{
-                        fl, nodep->fromp()->unlinkFrBack(), VCMethod::DYN_RESIZE,
-                        new AstVarRef{fl, sizeVarp, VAccess::READ}};
+                        fl, nodep->fromp()->unlinkFrBack(), VCMethod::DYN_RESIZE, sizeVarRefp};
                     resizep->dtypep(nodep->findVoidDType());
                     resizerTaskp->addStmtsp(new AstStmtExpr{fl, resizep});
                 }
@@ -6387,6 +6388,7 @@ class RandomizeVisitor final : public VNVisitor {
                 m_constraintp->addItemsp(createSizeGteZeroConstraint(fl, sizeVarp));
             }
             AstVarRef* const sizeVarRefp = new AstVarRef{fl, sizeVarp, VAccess::READ};
+            sizeVarRefp->classOrPackagep(VN_AS(sizeVarp->user2p(), NodeModule));
             sizeVarRefp->user1(true);
             nodep->replaceWith(sizeVarRefp);
             VL_DO_DANGLING(nodep->deleteTree(), nodep);
